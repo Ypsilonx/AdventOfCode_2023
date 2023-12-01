2e6223f602a0db80842872e3da87bf7d57b86033
ypsilonx <cibulec.tom@gmail.com>
2023-12-01 19:42:44 +0100

    .....

--- AdventOfCode_2023_1_day/1_12_2023.py
+++ AdventOfCode_2023_1_day/1_12_2023.py
@@ -2,7 +2,7 @@
 
 import re
 
-path_input_data = '1_12_2023_input.txt' # G:\\Locations_EU\\OST\\EU_S\\SVS\\01_Personal\\CCT\\AoC_2023\\1_12_2023_input.txt'
+path_input_data = 'AdventOfCode_2023_1_day\\1_12_2023_input.txt' # G:\\Locations_EU\\OST\\EU_S\\SVS\\01_Personal\\CCT\\AoC_2023\\1_12_2023_input.txt'
 
 def find_numbers(row):
     numbers = re.findall(r'\d', row)
@@ -16,59 +16,37 @@ with open(path_input_data, 'r') as file:
     for row in file:
         result = find_numbers(row)
         if result:
-            print(result)
+            #print(result)
             all_result.append(int(result))
             
-suma = (all_result)
+suma = sum(all_result)
 print(suma)
 
 # SECOND PART:
 
 import re
 
-path_input_data = '1_12_2023_input.txt' # G:\\Locations_EU\\OST\\EU_S\\SVS\\01_Personal\\CCT\\AoC_2023\\1_12_2023_input.txt'
+def find_numbers(row, numbers_str):
+    positions = []
+    for number, str_number in enumerate(numbers_str, start=1):
+        indexes = [ind.start() for ind in re.finditer(str_number, row)]
+        for index in indexes:
+            if index >= 0:
+                positions.append([index,str(number)])
+    return positions
 
-word_numbers = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
-}
+path_input_data = 'AdventOfCode_2023_1_day\\1_12_2023_input.txt'
+word_numbers = "one, two, three, four, five, six, seven, eight, nine".split(", ")
+digits = "123456789"
 
-def find_numbers(row):
-    numbers = re.findall(r'(?:' + '|'.join(word_numbers.keys()) + r'|\d+)', row)
-    
-    if numbers:
-        # Pokud jsou k dispozici čísla, vrátíme je jako tuple
-        first_number = word_numbers.get(numbers[0], None) or int(numbers[0][0])
-        print(first_number)
-        last_number = word_numbers.get(numbers[-1], None) or int(numbers[-1][-1])
-        print(last_number)
-        
-        if first_number is None:
-            first_number = int(numbers[0])
-            
-        if last_number is None:
-            last_number = int(numbers[-1])
-        
-        return first_number, last_number
-    else:
-        return None, None
-
-all_result = []
 with open(path_input_data, 'r') as file:
+    suma = 0
     for row in file:
-        result = find_numbers(row)
-        if result[0] is not None and result[1] is not None:
-            print(result)
-            result_1 = f'{result[0]}{result[1]}'
-            print(result_1)
-            all_result.extend(int(result_1))
-            
-suma = sum(all_result)
+        numbers_in_lines = []
+        numbers_in_lines += find_numbers(row, word_numbers) #find indexes of all "word numbers"
+        numbers_in_lines += find_numbers(row, digits) #find indexes of all "ciphers"
+        numbers_in_lines.sort() #sort list
+        result = int(numbers_in_lines[0][1] + numbers_in_lines[-1][1]) #get first and last number
+        suma += result
+
 print(suma)
