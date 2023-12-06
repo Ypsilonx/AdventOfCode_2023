1e880555da0ce853a0d6b5e560a1f3caa6d25a16
Ypsilonx <109464813+Ypsilonx@users.noreply.github.com>
2023-12-06 10:54:50 +0100

    result

---
+++ AdventOfCode_2023_6_day/6_12_2023_first_part.py
@@ -0,0 +1,39 @@
+﻿import pandas as pd
+import numpy as np
+
+soubor_s_daty = '6_12_2023_input.txt'
+
+def vypocet_moznosti(time, distance):
+    posibility_win_race = []
+    for i in range(0, time):
+        speed = i * 1
+        race = time - i
+        race_dist = race * speed
+        if race_dist > distance:
+            posibility_win_race.append(i)
+    
+    return len(posibility_win_race)
+
+def pocet_moznosti(data):
+    number_posibility = []
+    for i in data:
+        x, y = i
+        number_posibility.append(vypocet_moznosti(x, y))
+    # print(number_posibility)     
+    return np.prod(number_posibility)
+
+with open(soubor_s_daty, 'r') as file:
+    vstupni_data = [radek.strip() for radek in file.readlines() if radek.strip()]  
+
+result_dict = {}
+for line in vstupni_data:
+    parts = line.split()
+    key = parts[0].rstrip(':')
+    values = [int(value) for value in parts[1:]]
+    result_dict[key] = values
+# print(result_dict)
+
+result_pairs = list(zip(result_dict['Time'], result_dict['Distance']))
+# print(result_pairs)
+
+print(pocet_moznosti(result_pairs))
---
+++ AdventOfCode_2023_6_day/6_12_2023_second_part.py
@@ -0,0 +1,32 @@
+﻿
+soubor_s_daty = '6_12_2023_input.txt'
+
+def vypocet_moznosti(zavod):
+    pocet_moznosti = 1
+    for time, distance in zavod:
+        posibility_win_race = 0
+        for j in range(0, time + 1):
+            if time != 0:
+                start = (time - j) * j
+            if start > distance:
+                posibility_win_race += 1
+        pocet_moznosti = pocet_moznosti * posibility_win_race
+        
+    return pocet_moznosti
+
+with open(soubor_s_daty, 'r') as file:
+    vstupni_data = [radek.strip() for radek in file.readlines() if radek.strip()]  
+
+result_dict = {}
+for line in vstupni_data:
+    parts = line.split()
+    key = parts[0].rstrip(':')
+    values = [int(value) for value in parts[1:]]
+    str_numbers = [str(num) for num in values]
+    result_string = ''.join(str_numbers)
+    result_number = int(result_string)
+    result_dict[key] = result_number
+print(result_dict)
+
+
+print(vypocet_moznosti(zavod =[(result_dict['Time'], result_dict['Distance'])]))
